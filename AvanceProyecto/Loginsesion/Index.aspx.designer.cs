5d0b7485da704fb348afd33890aa8402ac1c1dd1
burgos092 <114007517+burgos092@users.noreply.github.com>
2024-05-13 10:28:01 -0500

    Add files via upload

---
+++ AvanceProyecto/Loginsesion/Index.aspx.designer.cs
@@ -0,0 +1,11 @@
+﻿-----------------------------------------------
+
+namespace Login_InfoToolsSV
+{
+    public partial class Index
+    {
+        protected global::System.Web.UI.HtmlControls.HtmlForm formulario_index;
+        protected global::System.Web.UI.WebControls.Label lblBienvenida;
+        protected global::System.Web.UI.WebControls.Button BtnCerrar;
+    }
+}
